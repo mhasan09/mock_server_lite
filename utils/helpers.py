31dfabb1447021dd4417b2df9445c1957481bf1e
Mahabubul Hasan <shourav6688@gmail.com>
2023-07-24 00:46:35 +0600

    chore: added logger obj in helper

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,22 +7,35 @@ def get_logger(name):
     return logging.getLogger(f"{settings.LOGGER_ROOT_NAME}.{name}")
 
 
+logger = get_logger(__name__)
+
+
 def parse_all_responses_data(data):
-    return [
-        {
-            "id": item.id,
-            "title": item.title,
-            "content": json.loads(item.content),
-            "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S.%f"),
-        }
-        for item in data
-    ]
+    try:
+        return [
+            {
+                "id": item.id,
+                "title": item.title,
+                "content": json.loads(item.content),
+                "created_at": item.created_at.strftime("%Y-%m-%d %H:%M:%S.%f"),
+            }
+            for item in data
+        ]
+
+    except Exception as e:
+        logger.debug({"error": repr(e)})
+        return None
 
 
 def parse_single_response_data(obj):
-    return {
+    try:
+        return {
             "id": obj.id,
             "title": obj.title,
             "content": json.loads(obj.content),
             "created_at": obj.created_at.strftime("%Y-%m-%d %H:%M:%S.%f"),
         }
+
+    except Exception as e:
+        logger.debug({"error": repr(e)})
+        return None
