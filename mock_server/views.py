82271d61cc01b88bb1d573d3335ead0143e77bb1
Mahabubul Hasan <shourav6688@gmail.com>
2023-07-26 12:03:44 +0600

    chore: added logger in views

--- mock_server/views.py
+++ mock_server/views.py
@@ -9,14 +9,29 @@ logger = get_logger(__name__)
 
 
 def get_all_responses(request):
-    responses = Response.objects.get_all_objects()
-    data = parse_all_responses_data(responses)
-    logger.debug({"data": data})
-    return JsonResponse(data, safe=False)
+    try:
+        responses = Response.objects.get_all_objects()
+        data = parse_all_responses_data(responses)
+        logger.debug({"data": data})
+        return JsonResponse(data, safe=False)
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return JsonResponse({"error": "An error occurred"}, status=500)
 
 
 def get_single_response(request, id):
-    response_obj = Response.objects.get_object(id=id)
-    data = parse_single_response_data(obj=response_obj)
-    logger.debug({"data": data})
-    return JsonResponse(data)
+    try:
+        response_obj = Response.objects.get_object(id=id)
+
+        if response_obj is None:
+            logger.warning(f"Response object with ID {id} not found.")
+            return JsonResponse({"error": "Response not found"}, status=404)
+
+        data = parse_single_response_data(obj=response_obj)
+        logger.debug({"data": data})
+        return JsonResponse(data, safe=False)
+
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return JsonResponse({"error": "An error occurred"}, status=500)
